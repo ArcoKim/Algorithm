5fc8d60fdc15906cd9a53136569e68dfc8fc8b5a
ArcoKim <arcomade12@gmail.com>
2023-11-30 13:03:14 +0900

    [Gold IV] Title: 연구소, Time: 880 ms, Memory: 121700 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,42 +1,38 @@
-import sys, copy
+import sys
+import copy
+from itertools import combinations
 input = sys.stdin.readline
 
-maps = []
-n,m = map(int, input().split())
-zeros = []
-for i in range(n):
-    now = list(map(int, input().split()))
-    maps.append(now)
-    for j in range(m):
-        if now[j] == 0:
-            zeros.append([i, j])
+def dfs(y, x):
+    new_area[y][x] = 2
+    yl = [-1, 0, 0, 1]
+    xl = [0, -1, 1, 0]
+    for i in range(4):
+        ny = y + yl[i]
+        nx = x + xl[i]
+        if 0 <= ny < n and 0 <= nx < m and not new_area[ny][nx]:
+            dfs(ny, nx)
 
-def dfs(i, j):
-    comb1 = [0, -1, 1, 0]
-    comb2 = [-1, 0, 0, 1]
-    for k in range(4):
-        ni = i + comb1[k]
-        nj = j + comb2[k]
-        if 0 <= ni < n and 0 <= nj < m and cpmaps[ni][nj] == 0:
-            cpmaps[ni][nj] = 2
-            dfs(ni, nj)
+n,m = map(int, input().split())
+area = [list(map(int, input().split())) for _ in range(n)]
+
+zeros = []
+virus = []
+for i in range(n):
+    for j in range(m):
+        if area[i][j] == 0:
+            zeros.append((i,j))
+        if area[i][j] == 2:
+            virus.append((i,j))
 
 result = 0
-for i in range(len(zeros)):
-    for j in range(i+1, len(zeros)):
-        for k in range(j+1, len(zeros)):
-            cpmaps = copy.deepcopy(maps)
-            cpmaps[zeros[i][0]][zeros[i][1]] = 1
-            cpmaps[zeros[j][0]][zeros[j][1]] = 1
-            cpmaps[zeros[k][0]][zeros[k][1]] = 1
-            for l in range(n):
-                for o in range(m):
-                    if cpmaps[l][o] == 2:
-                        dfs(l, o)
-            tmpR = 0
-            for l in range(n):
-                for o in range(m):
-                    if cpmaps[l][o] == 0:
-                        tmpR += 1
-            result = max(result, tmpR)
+for pair in combinations(zeros, 3):
+    new_area = copy.deepcopy(area)
+    new_area[pair[0][0]][pair[0][1]] = 1
+    new_area[pair[1][0]][pair[1][1]] = 1
+    new_area[pair[2][0]][pair[2][1]] = 1
+    for loc in virus:
+        dfs(loc[0], loc[1])
+    result = max(result, sum(x.count(0) for x in new_area))
+
 print(result)
